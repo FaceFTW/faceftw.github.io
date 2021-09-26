0cc560903154827adcdc1751aeb3ac9c8ef88f15
Alex Westerman <westeraj@rose-hulman.edu>
2021-09-26 18:23:22 -0400

    Work on App Bootstrap
    App Component (Bootstrap)
    =======================
    -Added code to register NerdFonts with Angular Material
    -Moved toolbar to Sidenav-Content
    -Updated Styling to have toolbar and router content to not conflict
    -Fixed Sidenav Styling
    -Began adding responsive elements to router content
    
    Project Changes
    =======================
    -Remove old stylesheets (these have been effectively migrated)
    -Removed lingering test harnesses (I live dangerously)
    
    Known Issues
    =======================
    -Mobile responsive Sidenav toggle brokey?? (low priority)
    -Toolbar/Document title don't update on route change (Investigating kinda)
    -Continuing migration from Bootstrap styling (Affects all components)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,64 @@
 import { MediaMatcher } from "@angular/cdk/layout";
 import { ChangeDetectorRef, Component, OnDestroy } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { ActivatedRoute } from "@angular/router";
+import { Title } from "@angular/platform-browser";
 
+const titleFragment = " - Alex Westerman";
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-    title = "portfolio";
+    title = "Home - Alex Westerman";
+    toolbarTitle: String = "Home";
+
     mobileQuery: MediaQueryList;
-    
+
     private _mobileQueryListener: () => void;
 
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-        this.mobileQuery = media.matchMedia("(max-width: 600px)");
+    constructor(
+        changeDetectorRef: ChangeDetectorRef,
+        media: MediaMatcher,
+        iconReg: MatIconRegistry,
+        private route: ActivatedRoute,
+        private titleServ: Title
+    ) {
+        //Stuff for responsive sidenav
+        this.mobileQuery = media.matchMedia("(max-width: 922px)");
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this._mobileQueryListener);
+
+        //Register NerdFonts with Angular Material
+        iconReg.registerFontClassAlias("nf");
+        iconReg.setDefaultFontSetClass("nf");
+    }
+
+    ngOnInit() {
+        //Subscribe to route changes to update toolbar header
+        //This is hard coded as there are unlikely to be many more pages
+        this.route.url.subscribe((urlSegs) => {
+            console.log("urlSegs :>> ", urlSegs);
+            switch (urlSegs[0].path) {
+                case "main":
+                    this.titleServ.setTitle("Home" + titleFragment);
+                    this.toolbarTitle = "Home";
+                    break;
+                case "projects":
+                    this.titleServ.setTitle("Projects" + titleFragment);
+                    this.toolbarTitle = "Projects";
+                    break;
+                case "resume":
+                    this.titleServ.setTitle("Resume" + titleFragment);
+                    this.toolbarTitle = "Resume";
+                    break;
+                case "about":
+                    this.titleServ.setTitle("About" + titleFragment);
+                    this.toolbarTitle = "About";
+                    break;
+            }
+        });
     }
 
     ngOnDestroy(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+//Routed Components
+import { Error404Component } from "./error404/error404.component";
 import { AboutComponent } from "./about/about.component";
 import { ResumeComponent } from "./resume/resume.component";
 import { HomeComponent } from "./home/home.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 //Angular Material Components
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatListModule } from "@angular/material/list";
-import { Error404Component } from './error404/error404.component';
+import { MatIconModule } from "@angular/material/icon";
 @NgModule({
     declarations: [AppComponent, AboutComponent, ResumeComponent, HomeComponent, ProjectsComponent, Error404Component],
     imports: [
@@ -23,6 +26,7 @@ import { Error404Component } from './error404/error404.component';
         MatSidenavModule,
         MatToolbarModule,
         MatListModule,
+        MatIconModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
