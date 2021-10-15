18cfd910fa85bff50d1938354fc4ea14377865c5
Alex Westerman <westeraj@rose-hulman.edu>
2021-10-14 21:57:54 -0400

    Resume Migration to JSON Schemas
    -Resume is now automated through JSON Schema
    --Added New JSON schema for Resume Content
    --Added relevant Typescript interfaces and component code
    --Moved all relevant data to resume.json
    -Added other schema scaffolds for other webpage portions.
    -Updated Angular Dependencies where relevant

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,37 @@
 import { Component, OnInit } from "@angular/core";
+import resumeData from "src/assets/json/resume.json";
+
+export interface Highlight {
+	icon: string;
+	stat: string;
+	statDescription: string;
+}
+
+export interface Skill {
+	categoryName: string;
+	lowSkill?: string[];
+	medSkill?: string[];
+	highSkill?: string[];
+}
+
+export interface Education {
+	name: string;
+	degree: string[];
+	degreeAddendum?: string;
+	graduationDate: string;
+	gpa?: { unweighted: number; weighted: number };
+	relCoursework: string[];
+	propCoursework?: string[];
+}
+
+export interface Experience {
+	name: string;
+	position: string;
+	timeEmployed: string;
+	terminationReason?: string;
+	location?: string;
+	responsibilities: string[];
+}
 
 @Component({
 	selector: "app-resume",
@@ -6,6 +39,11 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./resume.component.scss"],
 })
 export class ResumeComponent implements OnInit {
+	highlights: Highlight[] = resumeData.highlights;
+	skills: Skill[] = resumeData.skills;
+	education: Education[] = resumeData.education;
+	experience: Experience[] = resumeData.experience;
+
 	constructor() {}
 
 	ngOnInit(): void {}
