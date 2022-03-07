e0a66e05161605f3bf0155546f86a3854a47b734
Alex Westerman <westeraj@rose-hulman.edu>
2022-03-06 20:02:23 -0500

    Updating projects (got some cool ones)
    -Removed DOA (dead on arrival) projects and assets
    -Added new projects w/ assets
    -Home page now displays 6 featured project cards
    -Updated Resume Skills
    -Updated Related Coursework
    -Updated Proposed Coursework Slightly

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,11 @@ import { Project, ProjectsService } from "../projects/projects.service";
 import { ResponsiveUIService } from "../responsive-ui.service";
 
 const featuredProject1Id = 5;
-const featuredProject2Id = 1;
-const featuredProject3Id = 2;
+const featuredProject2Id = 11;
+const featuredProject3Id = 7;
+const featuredProject4Id = 1;
+const featuredProject5Id = 2;
+const featuredProject6Id = 12;
 
 const nullAssetPath = "/assets/img/no_asset.webp";
 const assetPath = "/assets/img/";
@@ -17,8 +20,12 @@ export class HomeComponent implements OnInit {
 	featuredProject1: Project;
 	featuredProject2: Project;
 	featuredProject3: Project;
+	featuredProject4: Project;
+	featuredProject5: Project;
+	featuredProject6: Project;
 
 	featured: Project[];
+	featured2: Project[];
 
 	isMobile: boolean;
 
@@ -27,7 +34,12 @@ export class HomeComponent implements OnInit {
 		this.featuredProject2 = projectSingleton.projects[featuredProject2Id];
 		this.featuredProject3 = projectSingleton.projects[featuredProject3Id];
 
+		this.featuredProject4 = projectSingleton.projects[featuredProject4Id];
+		this.featuredProject5 = projectSingleton.projects[featuredProject5Id];
+		this.featuredProject6 = projectSingleton.projects[featuredProject6Id];
+
 		this.featured = [this.featuredProject1, this.featuredProject2, this.featuredProject3];
+		this.featured2 = [this.featuredProject4, this.featuredProject5, this.featuredProject6];
 
 		this.isMobile = ui.isMobile;
 		this.ui.isMobile$.subscribe((isMobile) => (this.isMobile = isMobile));
