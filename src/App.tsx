08b72ab36c026b6cd46b1c53cb0bef46e3e1971f
Alex Westerman <westeraj@rose-hulman.edu>
2022-08-22 18:56:44 -0400

    Initial Route Scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import "./App.css";
+import AboutPanel from "./panels/About";
+import Error404Panel from "./panels/Error404";
+import MainPanel from "./panels/Main";
+import ProjectsPanel from "./panels/Projects";
+import ResumePanel from "./panels/Resume";
+
+function Layout() {
+	return (
+		<div>
+			<Outlet />
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<MainPanel />} />
+					<Route path="projects" element={<ProjectsPanel />} />
+					<Route path="resume" element={<ResumePanel />} />
+					<Route path="about" element={<AboutPanel />} />
+					<Route path="*" element={<Error404Panel />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/panels/Projects.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProjectsPanel() {
+	return <div>ProjectsPanel</div>;
+}
