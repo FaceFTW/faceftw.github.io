d9812262d78383b47dda261fe759efb93e2a8379
Alex Westerman <westeraj@rose-hulman.edu>
2023-02-19 19:51:29 -0500

    Cleanup Old Things

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,11 +1,10 @@
-import {Alert, Box, IconButton, Snackbar, Tooltip, Typography} from '@mui/material';
+import { Alert, Box, IconButton, Snackbar, Tooltip, Typography } from '@mui/material';
 import Image from 'mui-image';
 import React from 'react';
-import {FaEnvelope, FaGithub, FaKey, FaLinkedin, FaTwitter} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+import { FaEnvelope, FaGithub, FaKey, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Pfp from '../assets/img/pfp.webp';
 import gpgkey from '../assets/json/gpg_key.json';
-
 
 const SocialLinks = () => {
 	const [gpgCopiedShown, setGpgCopiedShown] = React.useState(false);
@@ -17,28 +16,31 @@ const SocialLinks = () => {
 
 	//Styles
 	const pictueBoxStyle = {
-		'borderRadius': '50%',
-		'border': '0.5rem solid #202020',
-		'width': '8.5rem',
-		'transition': 'all 0.3s ease-in-out',
-		'margin': 'auto',
-		'&:hover': {'borderColor': '#43a047', },
+		borderRadius: '50%',
+		border: '0.5rem solid #202020',
+		width: '8.5rem',
+		transition: 'all 0.3s ease-in-out',
+		margin: 'auto',
+		'&:hover': { borderColor: '#43a047' },
 	};
 	const nameStyle = {
 		textDecoration: 'none',
 		color: 'white',
 		margin: 'auto',
-		'&:hover': {color: 'white', textDecoration: 'none'}
+		'&:hover': { color: 'white', textDecoration: 'none' },
 	};
 
 	const pictureBox = (
-		<Box component={Link} to='/' sx={{
-			'display': 'flex',
-			'flexDirection': 'column',
-			'textDecoration': 'none'
-		}}>
+		<Box
+			component={Link}
+			to='/'
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				textDecoration: 'none',
+			}}>
 			<Box sx={pictueBoxStyle}>
-				<Image src={Pfp} style={{borderRadius: '50%', width: '7.5rem', }} />
+				<Image src={Pfp} duration={0} style={{ borderRadius: '50%', width: '7.5rem' }} />
 			</Box>
 			<Typography component='h6' variant='h6' sx={nameStyle}>
 				Alex Westerman
@@ -49,30 +51,38 @@ const SocialLinks = () => {
 	return (
 		<Box className='sidenav'>
 			{pictureBox}
-			<Box
-				sx={{display: 'flex', justifyContent: 'center'}}>
+			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
 				<Tooltip title='Twitter'>
-					<IconButton href='https://twitter.com/_FaceFTW'><FaTwitter /></IconButton>
+					<IconButton href='https://twitter.com/_FaceFTW'>
+						<FaTwitter />
+					</IconButton>
 				</Tooltip>
 				<Tooltip title='Github'>
-					<IconButton href='https://github.com/FaceFTW'><FaGithub /></IconButton>
+					<IconButton href='https://github.com/FaceFTW'>
+						<FaGithub />
+					</IconButton>
 				</Tooltip>
 				<Tooltip title='Email'>
-					<IconButton href='mailto:alex@faceftw.dev'><FaEnvelope /></IconButton>
+					<IconButton href='mailto:alex@faceftw.dev'>
+						<FaEnvelope />
+					</IconButton>
 				</Tooltip>
 				<Tooltip title='Linkedin'>
-					<IconButton href='https://www.linkedin.com/in/faceftw'><FaLinkedin /></IconButton>
+					<IconButton href='https://www.linkedin.com/in/faceftw'>
+						<FaLinkedin />
+					</IconButton>
 				</Tooltip>
 				<Tooltip title='GPG Key'>
-					<IconButton onClick={copyGpgKey}><FaKey /></IconButton>
+					<IconButton onClick={copyGpgKey}>
+						<FaKey />
+					</IconButton>
 				</Tooltip>
 			</Box>
 			<Snackbar
-				anchorOrigin={{vertical: 'top', horizontal: 'left'}}
+				anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
 				open={gpgCopiedShown}
 				autoHideDuration={6000}
-				onClose={() => setGpgCopiedShown(false)}
-			>
+				onClose={() => setGpgCopiedShown(false)}>
 				<Alert onClose={() => setGpgCopiedShown(false)} severity='info'>
 					Copied GPG Key to Clipboard
 				</Alert>
