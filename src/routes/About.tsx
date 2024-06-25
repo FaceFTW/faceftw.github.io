1602800f3dd2bf9c754f329a8a0780f8a06cf0e4
Alex Westerman <alex@faceftw.dev>
2024-06-25 17:51:26 -0400

    Migrate About page to shadcn/ui

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,25 +1,20 @@
-import { Box, Typography } from '@mui/material';
 import { motion } from 'framer-motion';
-import Image from 'mui-image';
 
 const workspaceImage = new URL('../assets/img/workspace.webp', import.meta.url).href;
 const setupImage = new URL('../assets/img/setupv2.webp', import.meta.url).href;
 
 export const AboutPanel = () => {
 	return (
-		<Box
-			component={motion.div}
+		<motion.div
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{ duration: 0.5 }}
-			sx={{ display: 'flex', flexDirection: 'column' }}>
-			<Box>
-				<Typography variant='h4' sx={{ m: '1rem' }}>
-					Hi! My name is Alex
-				</Typography>
-				<Box sx={{ display: 'flex', m: '1.25rem' }}>
-					<Box sx={{ width: { sm: '50%' } }}>
-						<p>
+			className='flex flex-col'>
+			<div>
+				<h1 className='m-4 text-4xl'>Hi! My name is Alex</h1>
+				<div className='m-5 flex flex-col md:flex-row'>
+					<div className='flex flex-col'>
+						<p className='pb-4 pl-4'>
 							&emsp;Since I was 12 years old, I&apos;ve been cultivating my interests in computers and technology. I
 							taught myself how to program, use breadboards, prototype basic electronic solutions, and
 							disassembling/salvaging old computers. Whether it was speed disassembling and reassembling TI-84s out of
@@ -29,7 +24,7 @@ export const AboutPanel = () => {
 							This has allowed me to perceive problems and projects from a multi-faceted perspective, and make guided
 							decisions to accelerate workflow.
 						</p>
-						<p>
+						<p className='pb-4 pl-4'>
 							&emsp;I created this portfolio to demonstrate my knowledge and showcase some of the projects, regardless
 							if academic, professional, or personal, that I am proud of. I intend for this to also be a way to show
 							what I am actively working on, updating it as new projects start or finish, keeping my self from the old
@@ -37,32 +32,28 @@ export const AboutPanel = () => {
 							software because reverse engineering source files and doing small tweaks was one of the ways I learned to
 							program, so most of my projects are open source under a permissive license (unless otherwise stated).
 						</p>
-						<p>
-							I love to explore new areas where technology can be integrated, and this is a passion I expect to be
+						<p className='pb-4 pl-4'>
+							&emsp;I love to explore new areas where technology can be integrated, and this is a passion I expect to be
 							pursuing for the rest of my life. Thanks for visiting and seeing what I have learned over the variety of
 							experiences I have gone through!
 						</p>
-					</Box>
-					<Box sx={{ display: 'flex', flexDirection: 'column', width: { sm: '50%' }, m: '1rem' }}>
-						<Image
+					</div>
+					<div className='m-4 flex flex-col'>
+						<img
 							src={workspaceImage}
 							alt='workspace'
-							style={{ width: '100%', objectFit: 'contain' }}
-							duration={500}
+							className='w-[100%] object-contain'
+							// duration={500}
 						/>
-						<Image
+						<img
 							src={setupImage}
 							alt='setup'
-							height={500}
-							style={{
-								marginTop: '1rem',
-								objectFit: 'contain',
-							}}
-							duration={500}
+							className='mt-4 h-[500px] object-contain'
+							// duration={500}
 						/>
-					</Box>
-				</Box>
-			</Box>
-		</Box>
+					</div>
+				</div>
+			</div>
+		</motion.div>
 	);
 };
