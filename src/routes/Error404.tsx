3178e0d58d177b0a2594fd78df2834cfdfb7853d
Alex Westerman <alex@faceftw.dev>
2024-06-26 19:53:14 -0400

    Migrate Routing Error Page

--- src/routes/Error404.tsx
+++ src/routes/Error404.tsx
@@ -1,27 +1,29 @@
-import { Box, Typography } from '@mui/material';
 import { motion } from 'framer-motion';
-import Image from 'mui-image';
 import { Link } from 'react-router-dom';
 
 export const Error404Panel = () => {
 	const imageNum = Math.floor(Math.random() * 19);
 	const imageSrc = new URL(`../assets/img/404/err_${imageNum}.webp`, import.meta.url).href;
 	return (
-		<Box
-			component={motion.div}
+		<motion.div
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{ duration: 0.5 }}
-			sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-			<Typography variant='h1'>Oops...</Typography>
-			<br />
-			<Typography variant='body1'>You seem to have navigated to a page that doesn&apos;t exist yet...</Typography>
-			<Typography variant='body1'>
-				You can <Link to='/'>go back to the home page</Link> or enjoy this funny meme I have:
-			</Typography>
-			<Box sx={{ margin: '1rem 0rem' }}>
-				<Image src={imageSrc} alt='404' width={400} duration={500} />
-			</Box>
-		</Box>
+			className='align-center flex flex-col text-center'>
+			<h1 className='m-8 text-4xl'>Oops...</h1>
+			<p>You seem to have navigated to a page that doesn&apos;t exist yet...</p>
+			<p>
+				You can{' '}
+				<Link to='/' className='text-primary underline'>
+					go back to the home page
+				</Link>{' '}
+				or enjoy this funny meme I have:
+			</p>
+			<div className='m-8 flex'>
+				<div className='mx-auto' />
+				<img src={imageSrc} alt='404 funny' width={400} />
+				<div className='mx-auto' />
+			</div>
+		</motion.div>
 	);
 };
