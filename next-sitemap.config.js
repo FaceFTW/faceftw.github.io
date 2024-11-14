8f79ad77d52e905fb7fd1baff6e1fe7e8e421278
Alex Westerman <alex@faceftw.dev>
2024-11-13 19:27:50 -0500

    Add automatic sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://faceftw.dev',
+    generateRobotsTxt: false,
+    changefreq: 'weekly',
+    output: 'export',
+};
