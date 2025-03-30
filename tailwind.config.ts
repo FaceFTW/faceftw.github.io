d5d1652710c5d9357d4c83ff544f06d59e549b88
Alex Westerman <alex@faceftw.dev>
2025-03-30 12:46:53 -0400

    Initial Tailwind 4 CSS migration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,63 +8,6 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            colors: {
-                background: 'hsl(var(--background))',
-                foreground: 'hsl(var(--foreground))',
-                card: {
-                    DEFAULT: 'hsl(var(--card))',
-                    foreground: 'hsl(var(--card-foreground))',
-                },
-                popover: {
-                    DEFAULT: 'hsl(var(--popover))',
-                    foreground: 'hsl(var(--popover-foreground))',
-                },
-                primary: {
-                    DEFAULT: 'hsl(var(--primary))',
-                    foreground: 'hsl(var(--primary-foreground))',
-                },
-                secondary: {
-                    DEFAULT: 'hsl(var(--secondary))',
-                    foreground: 'hsl(var(--secondary-foreground))',
-                },
-                muted: {
-                    DEFAULT: 'hsl(var(--muted))',
-                    foreground: 'hsl(var(--muted-foreground))',
-                },
-                accent: {
-                    DEFAULT: 'hsl(var(--accent))',
-                    foreground: 'hsl(var(--accent-foreground))',
-                },
-                destructive: {
-                    DEFAULT: 'hsl(var(--destructive))',
-                    foreground: 'hsl(var(--destructive-foreground))',
-                },
-                border: 'hsl(var(--border))',
-                input: 'hsl(var(--input))',
-                ring: 'hsl(var(--ring))',
-                chart: {
-                    '1': 'hsl(var(--chart-1))',
-                    '2': 'hsl(var(--chart-2))',
-                    '3': 'hsl(var(--chart-3))',
-                    '4': 'hsl(var(--chart-4))',
-                    '5': 'hsl(var(--chart-5))',
-                },
-                sidebar: {
-                    DEFAULT: 'hsl(var(--sidebar-background))',
-                    foreground: 'hsl(var(--sidebar-foreground))',
-                    primary: 'hsl(var(--sidebar-primary))',
-                    'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-                    accent: 'hsl(var(--sidebar-accent))',
-                    'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-                    border: 'hsl(var(--sidebar-border))',
-                    ring: 'hsl(var(--sidebar-ring))',
-                },
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)',
-            },
             //NOTE Color Values are based on Tailwind Calculated Values
             backgroundImage: {
                 'gradient-init': '',
@@ -82,22 +25,6 @@ const config: Config = {
                     'linear-gradient(to right, #0c0a09 50%,transparent 50%), linear-gradient(to right,transparent 50%,#818cf8 50%,#06b6d4 75%)',
             },
             keyframes: {
-                'accordion-down': {
-                    from: {
-                        height: '0',
-                    },
-                    to: {
-                        height: 'var(--radix-accordion-content-height)',
-                    },
-                },
-                'accordion-up': {
-                    from: {
-                        height: 'var(--radix-accordion-content-height)',
-                    },
-                    to: {
-                        height: '0',
-                    },
-                },
                 'gradient-trans': {
                     from: {
                         backgroundPosition: '0%',
@@ -108,8 +35,6 @@ const config: Config = {
                 },
             },
             animation: {
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
                 'gradient-trans': 'gradient-trans 1.5s ease-in-out',
             },
             typography: {
@@ -123,6 +48,5 @@ const config: Config = {
             },
         },
     },
-    plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
 export default config;
