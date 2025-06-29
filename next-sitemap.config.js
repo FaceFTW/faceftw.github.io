36ac7b690bc0f39680449e5b0efd09833ef2b660
Alex Westerman <alex@faceftw.dev>
2025-06-29 19:35:05 -0400

    A _very_ early port to 11ty

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://faceftw.dev',
-    generateRobotsTxt: false,
-    changefreq: 'weekly',
-    output: 'export',
-};
