eea8c6d4896fa3aae5b9f9447eacd9ac3b2ff864
Alex Westerman <alex@faceftw.dev>
2025-07-31 21:19:11 -0400

    Add Shut Up Device to Projects

--- src/_data/featuredProjects.mjs
+++ src/_data/featuredProjects.mjs
@@ -3,7 +3,7 @@ import projects from './projects.json' with { type: 'json' };
 export default function () {
     return projects.projectList
         .filter((project) => {
-            return project.projectId === 18 || project.projectId === 11 || project.projectId === 5;
+            return project.projectId === 18 || project.projectId === 11 || project.projectId === 19;
         })
         .sort((a, b) => {
             return b.projectId - a.projectId;
